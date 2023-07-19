d92025b5f3f457d8a2442dc9efd3d14a88d59c25
Wicaro <wicaromendes@gmail.com>
2023-07-19 19:35:56 -0300

    short name sentry.json

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const path = require('path');
 const fs = require('fs');
 const chalk = require('chalk');
 
-const currentDirectory = process.cwd().replace('/\\/g','/');
-const OneSentryPath = path.join(currentDirectory, 'Sentry.json');
+const currentDirectory = process.cwd()
+const OneSentryPath = path.join(currentDirectory, 'sentry.json');
 
 if (!fs.existsSync(OneSentryPath)) {
-    console.log(chalk.red("[OneSentry] - The OneSentry configuration file (Sentry.json) was not created in the root of the project. Create the file and try again."))
+    console.log(chalk.red("[OneSentry] - The OneSentry configuration file (sentry.json) was not created in the root of the project. Create the file and try again."))
     console.log(chalk.red("[OneSentry] - Documentation: https://github.com/wicaromendes/one-sentry"))
 }else{
     Monitoring(OneSentryPath)
