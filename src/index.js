98b8ad556d765f84fbcf96069837ab341e45be85
Wicaro <wicaromendes@gmail.com>
2023-07-19 19:26:10 -0300

    update message not found file Sentry.json

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const currentDirectory = process.cwd().replace('/\\/g','/');
 const OneSentryPath = path.join(currentDirectory, 'Sentry.json');
 
 if (!fs.existsSync(OneSentryPath)) {
-    console.log(chalk.red("[OneSentry] - The OneSentry configuration file was not created in the root of the project. Create the file and try again."))
+    console.log(chalk.red("[OneSentry] - The OneSentry configuration file (Sentry.json) was not created in the root of the project. Create the file and try again."))
     console.log(chalk.red("[OneSentry] - Documentation: https://github.com/wicaromendes/one-sentry"))
 }else{
     Monitoring(OneSentryPath)
