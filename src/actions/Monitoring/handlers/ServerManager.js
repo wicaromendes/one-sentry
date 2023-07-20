9b79ccb24f8da15c4ea5a8484944d410de879969
Wicaro <wicaromendes@gmail.com>
2023-07-19 23:38:46 -0300

    Fix: Kill process

--- src/actions/Monitoring/handlers/ServerManager.js
+++ src/actions/Monitoring/handlers/ServerManager.js
@@ -1,12 +1,13 @@
 const chalk = require("chalk");
 const { spawn } = require('child_process');
 const path = require('path');
+const terminate = require('terminate');
 
 let serverProcess = null;
 
 const startServer = (OneSentryPath, OneSentryCommand) => {
     console.log(chalk.green(`[OneSentry] - Starting command: ${chalk.bgMagenta(OneSentryCommand)}`));
-    const serverProcess = spawn(OneSentryCommand, { stdio: 'inherit', cwd:OneSentryPath, shell: process.platform == 'win32' });
+    serverProcess = spawn(OneSentryCommand, { stdio: 'inherit', cwd:OneSentryPath, shell: process.platform == 'win32' });
 
     serverProcess.on('error', (error) => {
         console.log(chalk.red(`[OneSentry] - Error executing file ${error}`));
@@ -14,11 +15,20 @@ const startServer = (OneSentryPath, OneSentryCommand) => {
 };
 
 const restartServer = (OneSentryPath, OneSentryCommand) => {
-    if (serverProcess) {
-        console.log(chalk.yellow('[OneSentry] - Restarting server...'));
-        serverProcess.kill();
+    console.log(chalk.yellow('[OneSentry] - Restarting server...'));
+    if(serverProcess){
+        terminate(serverProcess.pid, function (err) {
+            if (err) { // you will get an error if you did not supply a valid process.pid
+                console.log(chalk.red(`[OneSentry] - Error executing file ${err}`)); // handle errors in your preferred way.
+            }
+            else {
+                console.log(chalk.yellow('[OneSentry] - Server restarted...'));
+                startServer(OneSentryPath, OneSentryCommand)
+            }
+        });
+    }else{
+        startServer(OneSentryPath, OneSentryCommand)
     }
-    startServer(OneSentryPath, OneSentryCommand)
 };
 
 module.exports = { startServer, restartServer };
